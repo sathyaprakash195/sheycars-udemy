e4cf0649cb1202015a9687d5da82ebed0031691f
sathyaprakash195 <sathyaprakash195@gmail.com>
2021-10-24 10:47:54 +0530

    commit

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -22,6 +22,7 @@ function Register() {
       <Row gutter={16} className="d-flex align-items-center">
         <Col lg={16} style={{ position: "relative" }}>
           <img 
+           className='w-100'
            data-aos='slide-left'
            data-aos-duration='1500'
           src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" />
